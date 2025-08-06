927ed4fd5fd606f002e44fd6920973db99add448
nvminh162 <nvminh162@gmail.com>
2025-08-06 11:26:02 +0700

    Remove author comments from UserController, User, UserRepository, and UserService files

--- project-x/project-x-backend/src/main/java/com/nvminh162/projectxbackend/repository/UserRepository.java
+++ project-x/project-x-backend/src/main/java/com/nvminh162/projectxbackend/repository/UserRepository.java
@@ -1,14 +1,3 @@
-/*
- * Author: Hỏi Dân IT - @hoidanit 
- *
- * This source code is developed for the course
- * "Java Spring RESTful APIs - Xây Dựng Backend với Spring Boot".
- * It is intended for educational purposes only.
- * Unauthorized distribution, reproduction, or modification is strictly prohibited.
- *
- * Copyright (c) 2025 Hỏi Dân IT. All Rights Reserved.
- */
-
 package com.nvminh162.projectxbackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
